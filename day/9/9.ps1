66d39f953dd902492716931bbf8e04aa36251f51
Maciej <maciek.urban+github@gmail.com>
2022-12-10 04:09:12 +0100

    D9.5

--- day/9/9.ps1
+++ day/9/9.ps1
@@ -1,31 +1,52 @@
 #functions
-function MoveHead {
+function ConvertToVector {
     param (
-        $current,
+        $position,
         $move
     )
 
     switch ($move[0]) {
-        R { $current[0] += $move[1] }
-        L { $current[0] -= $move[1] }
-        U { $current[1] += $move[1] }
-        D { $current[1] -= $move[1] }
+        R { $position[0] += $move[1] }
+        L { $position[0] -= $move[1] }
+        U { $position[1] += $move[1] }
+        D { $position[1] -= $move[1] }
     }
 
-    return $current
+    return $position
 
 }
 
-function Move {
+function MakeMove {
     param (
         $head,
         $tail,
         $move
     )
+    #head always moves in single direction
+    if(($move[0]+$move[1]) -lt 0){
+        $multiplier = -1
+    }
+    $multiplier = 1
 
-    #for ()
-    
-    $matrix[$tail[0], $tail[1]] = 1
+
+    for ($x = 0; $x -lt $move[0]; $x++){
+        $head[0] += $multiplier
+        
+
+        
+        $matrix[$head[0], $head[1]] = 1
+        $matrix[$tail[0], $tail[1]] = 1
+        
+    }
+    for ($y = 0; $y -lt $move[1]; $y++){
+        $head[1] += $multiplier
+
+        $matrix[$head[0], $head[1]] = 1
+        $matrix[$tail[0], $tail[1]] = 1
+    }
+    return $head, $tail
+
+
 }
 
 function ReadOperations {
@@ -54,7 +75,7 @@ function GetMaxDimensions {
     $startingPoint = @(0,0)
 
     foreach ($row in $op){
-        $startingPoint = MoveHead $startingPoint $row
+        $startingPoint = ConvertToVector $startingPoint $row
         $H += $startingPoint[0]
         $V += $startingPoint[1]
     }
@@ -78,8 +99,8 @@ function main {
     $head = @(0, 0)
     $tail = @(0, 0)
 
-    foreach ($o in $op){
-        MoveHead $head $o
+    foreach ($movement in $op){
+        MakeMove $head $tail $(ConvertToVector @(0,0) $movement)
         
     }
     $matrix
